185633d37e045ecee16f81ab14c3fc1e936f6b0e
Shengyi ZHOU <60843629+LeoLionly5@users.noreply.github.com>
2021-03-07 04:20:34 +0100

    Update mysql.js

--- back-end/middlewares/mysql.js
+++ back-end/middlewares/mysql.js
@@ -1,25 +1,12 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-const sqlConfig = {
+const dbConnection = mysql.createConnection({
     host: process.env.MYSQL_HOST,
     port: process.env.MYSQL_PORT,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-}
+    database: process.env.MYSQL_DATABASE,
+});
 
-var dbConnection = function(){
-    let connection = mysql.createConnection(sqlConfig)
-    connection.connect()
-    connection.on('error',err=>{
-        console.log('Re-connecting lost connection: ');
-        connection = mysql.createConnection(sqlConfig)
-
-    })
-    return function(){
-        return connection
-    }
-}
-
-module.exports = dbConnection();
+module.exports = dbConnection;
