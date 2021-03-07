62456a7f3bcea58c789bd57407d3e63f26169d38
Shengyi ZHOU <60843629+LeoLionly5@users.noreply.github.com>
2021-03-07 21:36:00 +0100

    Add files via upload

---
+++ back-end/test/app.spec.js
@@ -0,0 +1,41 @@
+var supertest = require("supertest");
+var should = require("should");
+require('dotenv').config();
+
+var server = supertest.agent("http://localhost:"+process.env.BACKEND_PORT);
+
+describe("SAMPLE unit test", function () {
+    it("should work", function (done) {
+        server
+        .get("/")
+        .expect(200)
+        .end(function (err, res) {
+            res.status.should.equal(200);
+            done();
+        });
+    });
+
+    it("should initialize the DB", function (done) {
+        server
+        .get("/api")
+        .expect(200) // THis is HTTP response
+        .end(function (err, res) {
+            // HTTP status should be 200
+            res.status.should.equal(200);
+            console.log(res.body);
+            done();
+        });
+    });
+
+    it("should get categories", function (done) {
+        server
+        .get("/api/getAllCategories")
+        .expect(200)
+        .end(function (err, res) {
+            res.status.should.equal(200);
+            console.log(res.body);
+            done();
+        });
+    });
+
+});
